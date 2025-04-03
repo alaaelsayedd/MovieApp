4432c31451c6a2c88d37184714f6520bdb130706
Alaa Elsayed <salaaelsayed@gmail.com>
2025-04-04 01:15:15 +0200

    update navbar route

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  lang: string[] = ['EN', 'AR'];
+  selected_lang = 'EN';
 }
