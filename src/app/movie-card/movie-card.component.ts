8f58d7364639ae381c261865f026d2631d3623d6
Alaa Elsayed <salaaelsayed@gmail.com>
2025-04-04 05:37:18 +0200

    add Pagnation to Home Page

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -10,4 +10,15 @@ import { RouterLink } from '@angular/router';
 })
 export class MovieCardComponent {
   @Input() movie: any;
+
+  getStrokeDashArray(rating: number): string {
+    const percentage = rating * 10;
+    return `${percentage} 100`;
+  }
+
+  getRatingColor(rating: number): string {
+    if (rating >= 7) return '';
+    if (rating >= 5) return 'yellow';
+    return 'red';
+  }
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { CommonModule } from '@angular/common';
 import { Movie } from '../types/movie';
@@ -10,18 +10,45 @@ import { MovieApiService } from '../services/movie-api.service';
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css',
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
   movies: Movie[] = [];
   page: number = 1;
   totalPages: number = 0;
   movieRequestService = inject(MovieApiService);
+
   ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
     this.movieRequestService.getMovies(this.page).subscribe((data) => {
       this.movies = data.results;
       this.page = data.page;
-      this.totalPages = data.total_pages;
-      console.log(this.movies);
-      console.log(this.page);
+      this.totalPages = Math.min(data.total_pages, 500);
     });
+  }
+
+  changePage(newPage: number) {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.page = newPage;
+      this.loadMovies();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+
+  getVisiblePages(): number[] {
+    const maxVisiblePages = 5;
+    let startPage = Math.max(1, this.page - Math.floor(maxVisiblePages / 2));
+    let endPage = startPage + maxVisiblePages - 1;
+
+    if (endPage > this.totalPages) {
+      endPage = this.totalPages;
+      startPage = Math.max(1, endPage - maxVisiblePages + 1);
+    }
+
+    return Array.from(
+      { length: endPage - startPage + 1 },
+      (_, i) => startPage + i
+    );
   }
 }
