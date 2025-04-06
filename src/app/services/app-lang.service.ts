f5594a1ec337dc107f3b00f46546562a528b8e33
Alaa Elsayed <salaaelsayed@gmail.com>
2025-04-06 08:33:19 +0200

    change direction with language

--- src/app/services/app-lang.service.ts
+++ src/app/services/app-lang.service.ts
@@ -6,12 +6,22 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AppLangService {
   private lang = new BehaviorSubject<String>('en');
-
+  private direction = new BehaviorSubject<String>('ltr');
   constructor() {}
   getAppLang() {
     return this.lang.asObservable();
   }
   setAppLang(lang: string) {
     this.lang.next(lang);
+    this.setAppDirection(lang);
+  }
+  getAppDirection() {
+    return this.direction.asObservable();
+  }
+
+  private setAppDirection(lang: string) {
+    // Logic to determine direction based on language
+    const dir = lang === 'ar' ? 'rtl' : 'ltr';
+    this.direction.next(dir);
   }
 }
