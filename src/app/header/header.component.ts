ab1f227da3e6f679e67d0a753bb58b510902d091
iman <iman@127.0.1.1iman-pc.localiman-pc>
2025-04-06 03:08:25 +0200

    search done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MovieApiService } from '../services/movie-api.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  searchQuery: string = '';
+  searchResults: any[] = [];
+  imgBaseUrl: string = 'https://image.tmdb.org/t/p/w500'; 
+  constructor(private movieApiService: MovieApiService) {}
 
+  onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.movieApiService.searchMovies(this.searchQuery).subscribe({
+        next: (response) => {
+          if (response && response.results && response.results.length > 0) {
+            this.searchResults = response.results;
+          } else {
+            this.searchResults = [];
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching search results:', err);
+          this.searchResults = [];
+        }
+      });
+    } else {
+      this.searchResults = [];
+    }
+  }
 }
